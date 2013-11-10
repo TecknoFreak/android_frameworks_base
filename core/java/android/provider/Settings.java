15afa28cdd5cf9038706cee9b730d8408954efe0
maxwen <max.weninger@gmail.com>
2013-11-10 13:17:37 +0100

    base: show notification count overlay
    Change-Id: Ie4df927531bd8d2869780336b5657f8194141b42
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2839,6 +2839,12 @@ public final class Settings {
         public static final String PSX_LOCKSCREEN_QUICK_UNLOCK_CONTROL = "psx_lockscreen_quick_unlock_control";
 		
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
