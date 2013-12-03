fd52133f90bc0fee31457cd7d50d20e4e3f2011c
Kyrylo Mikos <kiril.mik.os@gmail.com>
2013-12-03 19:16:41 +0200

    Update immersive confirmation.
    1. Disable panic mechanism for expanded desktop.
    2. Separate confirmation for each expanded desktop style.
    3. Add new layout for the case when navigation
       bar is hidden but statusbar is visible.
    
    PS1: Adapt to our immersive and fix conflicts.
    PS2: ???
    PS3: Hopefully restore previous state.
    PS4: Spanish translations
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>
    Change-Id: I5914d655bd5f9315ac4cc6fc7d70cc4ee45bb7b2
    
    Conflicts:
    	core/res/res/values-es/pa_strings.xml
    	core/res/res/values/pa_strings.xml

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -4067,8 +4067,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case KeyEvent.KEYCODE_POWER: {
                 result &= ~ACTION_PASS_TO_USER;
                 if (down) {
-                    mImmersiveModeConfirmation.onPowerKeyDown(isScreenOn, event.getDownTime(),
-                            isImmersiveMode(mLastSystemUiFlags));
+                    if (mImmersiveModeStyle == 0) {
+                        mImmersiveModeConfirmation.onPowerKeyDown(isScreenOn, event.getDownTime(),
+                                isImmersiveMode(mLastSystemUiFlags));
+                    }
                     if (isScreenOn && !mPowerKeyTriggered
                             && (event.getFlags() & KeyEvent.FLAG_FALLBACK) == 0) {
                         mPowerKeyTriggered = true;
@@ -5350,8 +5352,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         boolean oldImmersiveMode = isImmersiveMode(oldVis);
         boolean newImmersiveMode = isImmersiveMode(vis);
         if (win != null && oldImmersiveMode != newImmersiveMode) {
-            final String pkg = mImmersiveModeStyle != 0 ? "android" : win.getOwningPackage();
-            mImmersiveModeConfirmation.immersiveModeChanged(pkg, newImmersiveMode);
+            final String pkg = mImmersiveModeStyle != 0 ? "android" + mImmersiveModeStyle
+                    : win.getOwningPackage();
+            mImmersiveModeConfirmation.immersiveModeChanged(pkg, newImmersiveMode,
+                    transientStatusBarAllowed);
         }
 
         vis = mNavigationBarController.updateVisibilityLw(transientNavBarAllowed, oldVis, vis);
