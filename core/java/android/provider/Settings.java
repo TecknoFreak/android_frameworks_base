dc285d21c88c098205ccd41ac4d1663fe6dea66f
Martin Rodemann <martin@rodemann.org>
2014-01-05 18:33:59 +0100

    Fixes for some derps

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2556,10 +2556,10 @@ public final class Settings {
         public static final String EGG_MODE = "egg_mode";
 
         /**
-         * Whether to mute annoying notifications
-         * @hide
-         */
-        public static final String ANNOYING_NOTIFICATIONS_THRESHOLD =
+        * Whether to mute annoying notifications
+        * @hide
+        */
+        public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD =
                 "mute_annoying_notifications_threshold";
 
         /**
